22e92b04be70a358f4ea87460ccb2c48a474641f
hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu <58358771+hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu@users.noreply.github.com>
2023-08-15 17:21:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,28 @@ function toggleRandomOption() {
   randomOptionEnabled = !randomOptionEnabled;
   clearRandomNumber();
 }
-
+//Debut
 function startGame() {
   player1Name = document.getElementById("player1").value;
   player2Name = document.getElementById("player2").value;
 
   document.getElementById("player-names").style.display = "none";
   document.getElementById("game-frame").style.display = "block";
+ hideButton();
   document.getElementById("player-turn").textContent = "Turn: " + player1Name;
 }
+// Affiche les deux boutons Pass et skip
+function showButton() {
+  document.getElementById("skipButton").style.display = "block";
+  document.getElementById("passButton").style.display = "block";
+}
+// Enleve les deux boutons Pass et skip
+function hideButton() {
+  document.getElementById("skipButton").style.display = "none";
+document.getElementById("passButton").style.display = "none";
+}
 
+// Question
 function getRandomPhrase() {
   var phrases = [
     "What is your favorite color?",
@@ -26,7 +38,7 @@ function getRandomPhrase() {
   var randomIndex = Math.floor(Math.random() * phrases.length);
   return phrases[randomIndex];
 }
-
+// Action
 function getRandomAction() {
   var phrases = [
     "NNO",
@@ -37,17 +49,17 @@ function getRandomAction() {
   var randomIndex = Math.floor(Math.random() * phrases.length);
   return phrases[randomIndex];
 }
-
+// Affichage question
 function displayQuestion(phrase) {
   document.getElementById("questionText").textContent = "Question: " + phrase;
   document.getElementById("actionText").textContent = "";
 }
-
+//Affichage reponse
 function displayAction(phrase) {
   document.getElementById("questionText").textContent = "";
   document.getElementById("actionText").textContent = "Action: " + phrase;
 }
-
+// Génération du nombre
 function getRandomNumber() {
   if (randomOptionEnabled) {
     return Math.floor(Math.random() * 4);
@@ -57,44 +69,49 @@ console.log('Nombre non générer');
     return "";
   }
 }
-
+// Execution des question
 function poseQuestion() {
   var phrase = getRandomPhrase();
+ showButton();
   displayQuestion(phrase);
   clearRandomNumber();
 }
-
+// Execution des action
 function requestAction() {
   var phrase = getRandomAction();
+ showButton();
   displayAction(phrase);
   clearRandomNumber();
 }
-
+// Affichage du nombre de gorgé
 function displayRandomNumber(playerName) {
   var randomNumber = getRandomNumber();
   var message = playerName + " doit prendre " + randomNumber;
   document.getElementById("randomNumberMessage").textContent = message;
 }
 
+// Enleve le nombre de gorgé
 function clearRandomNumber() {
   document.getElementById("randomNumberMessage").textContent = "";
 }
-
+// Skip la question/action 
 function skipProposition() {
   var currentPlayerName = currentPlayer === 1 ? player1Name : player2Name;
   document.getElementById("actionText").textContent = "";
 document.getElementById("questionText").textContent = "";
+ hideButton();
   if (randomOptionEnabled) {
     displayRandomNumber(currentPlayerName);
   }
   currentPlayer = currentPlayer === 1 ? 2 : 1;
   document.getElementById("player-turn").textContent = "Turn: " + (currentPlayer === 1 ? player1Name : player2Name);
 }
-
+// Passe la question/action
 function passProposition() {
   var currentPlayerName = currentPlayer === 1 ? player1Name : player2Name;
   document.getElementById("actionText").textContent = "";
 document.getElementById("questionText").textContent = "";
+   hideButton();
   currentPlayer = currentPlayer === 1 ? 2 : 1;
   document.getElementById("player-turn").textContent = "Turn: " + (currentPlayer === 1 ? player1Name : player2Name);
 }
